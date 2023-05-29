1068d402732f6c30865a008f6e2ad8ef5fa84257
FaizBShah <faizbshah2001@gmail.com>
2023-05-29 12:03:16 +0530

    Wrote the auth service logic

--- auth-service/services/auth-service.go
+++ auth-service/services/auth-service.go
@@ -3,9 +3,14 @@ package services
 import (
 	"auth-service/models"
 	"auth-service/utils"
+	"errors"
 )
 
 func RegisterUser(name string, email string, password string, userType models.UserType) (*models.User, error) {
+	if user, _ := models.FindUserByEmail(email); user != nil {
+		return nil, errors.New("user is already registered")
+	}
+
 	hashedPassword, err := utils.GenerateHashFromPassword(password)
 
 	if err != nil {
@@ -20,4 +25,28 @@ func RegisterUser(name string, email string, password string, userType models.Us
 	}
 
 	return models.CreateUser(&newUser)
+}
+
+func LoginUser(email string, password string) (string, error) {
+	user, err := models.FindUserByEmail(email)
+
+	if err != nil {
+		return "", errors.New("user's account does not exist")
+	}
+
+	if !utils.ValidatePassword(user.Password, password) {
+		return "", errors.New("email/password is invalid")
+	}
+
+	jwtToken, err := utils.GenerateJwtToken(*user)
+
+	if err != nil {
+		return "", err
+	}
+
+	return jwtToken, nil
+}
+
+func AuthenticateUser(token string) (*utils.JwtClaims, error) {
+	return utils.ValidateJwtToken(token)
 }
